b0631b9bb22a91c144195de0173787fd9c1fa3d5
maxim.ai <aidinmaxim85@gmail.com>
2023-11-19 16:28:20 +0100

    first commit

---
+++ src/main/Game.java
@@ -0,0 +1,68 @@
+package main;
+
+public class Game implements Runnable {
+    private GameWindow gameWindow;
+    private GamePanel gamepanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
+
+    public Game() {
+        gamepanel = new GamePanel();
+        gameWindow = new GameWindow(gamepanel);
+        gamepanel.requestFocus();
+        startGameLoop();
+    }
+
+    public void update() {
+        gamepanel.updateGame();
+    }
+
+    private void startGameLoop() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1_000_000_000.0 / FPS_SET;
+        double timePerUpdate = 1_000_000_000.0 / UPS_SET;
+
+        long previousTime = System.nanoTime();
+
+        int frames = 0;
+        int updates = 0;
+        long lastCheck = System.currentTimeMillis();
+
+        double deltaU = 0;
+        double deltaF = 0;
+
+        while (true) {
+            long currentTime = System.nanoTime();
+
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
+
+            if (deltaU >= 1) {
+                update();
+                updates++;
+                deltaU--;
+            }
+
+            if (deltaF >= 1) {
+                gamepanel.repaint();
+                frames++;
+                deltaU--;
+            }
+
+            if (System.currentTimeMillis() - lastCheck >= 1000) {
+                lastCheck = System.currentTimeMillis();
+                System.out.println("FPS: " + frames + " | UPS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+    }
+}
